f33b8b8cff16d4c991d68d0a36a77486e98df59d
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-03-31 04:41:02 +0530

    Add assignment4.py

---
+++ Assignments/assignment4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+counter = 1
+
+
+def dolots(count):
+    global counter
+    for i in (1, 2, 3):
+        counter = count + i
+
+
+print(dolots(4))
+# Output: None
+print(counter)
+# Output: 7
