d1e2eec2b07fa45b0517f3826ead3bf15edcedda
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-03-31 04:41:02 +0530

    Add assignment3.py

---
+++ Assignments/assignment3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+print(True if input("Enter a string: ").count('e') > 1 else False)
+# str1=input("Enter a string : ")
+# count=0
+# strlen=len(str1)
+# for i in range(0,strlen):
+#     if (str1[i]=='e'):
+#         count=count+1
+# if count>1:
+# 	print ("True")
+# else:
+#     print("False")
