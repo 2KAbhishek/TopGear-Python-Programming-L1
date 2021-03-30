8a4f220a1950ec0fbae10911892d3fa0db727035
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-03-31 04:41:01 +0530

    Add assignment1.py

---
+++ Assignments/assignment1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+mylist = list(range(4))
+seclist = mylist
+print(seclist)
+# Output: [0, 1, 2, 3]
+mylist.append(4)
+print(seclist)
+# Output: [0, 1, 2, 3, 4]
+seclist = mylist[:]
+print(seclist)
+# Output: [0, 1, 2, 3, 4]
+mylist.append(5)
+print(seclist)
+# Output: [0, 1, 2, 3, 4]
